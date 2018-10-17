7bd5fa9d6c8d46247c8ba8c3d390b7adad61f636
thisu96 <thisarudinuwanthi@gmail.com>
2018-10-17 20:19:48 +0530

    react first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+      <div className="row">
+      <div className="col-md-8">
+      <form>
+        <fieldset>
+          <div className="col-md-10">
+        <legend>SEARCH APP</legend>
+          <div className="form-group row">
+            <label htmlFor="searchPlace" className="col-sm-2 col-form-label">Place</label>
+              <div className="col-sm-5">
+                <input type="text" className="form-control-plaintext" id="searchPlace" value="Search Place"></input>
+              </div>
+            </div>
+            </div>
+            <div className="col-md-2">
+            <button className="btn btn-primary">SEARCH</button>
+              </div>
+        </fieldset>
+      </form>
+      </div>
+
+      <div className="col-md-4">
+      <legend>Save Places</legend>
+
+        </div>
+
+      </div>
       </div>
     );
   }
